18a9042d2c987bb8e4a5ea0bb072718674d1f90d
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2023-10-20 20:35:15 +0100

    initial commit

---
+++ terminal.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+const { exec } = require('child_process');
+
+
+app.get('/restart', function (req, res) {
+  // Replace 'YOUR_COMMAND_HERE' with the actual command you want to run
+  const command = 'pm2 restart 0';
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing the command: ${error}`);
+      return;
+    }
+
+    console.log(`Command output: ${stdout}`);
+    console.error(`Command errors: ${stderr}`);
+  });
+
+  res.send('<h1>Bot restarted successfully</h1>')
+})
+
+const port = 8080
+
+var server = app.listen(port, function () {
+   console.log("app listening at port " + port)
+})
